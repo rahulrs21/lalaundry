0beaed2858d95162c827798a7f2cfac97114cf8e
ProPixel <studio@propixel.ae>
2022-10-01 17:24:53 +0400

    sixth commit

--- pages/post.tsx
+++ pages/post.tsx
@@ -1,6 +1,5 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import React from 'react'
 import Navbar from '../components/Navbar'
 import { sanityClient, urlFor } from '../sanity';
 import { Post } from '../typings';
