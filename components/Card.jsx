6a22b4752a6e30419f6b9a9cc05d9d41e46b9c1c
ProPixel <studio@propixel.ae>
2022-10-03 17:14:07 +0400

    test1

--- components/Card.jsx
+++ components/Card.jsx
@@ -5,6 +5,12 @@ import OpacityIcon from '@mui/icons-material/Opacity';
 import LocalLaundryServiceIcon from '@mui/icons-material/LocalLaundryService';
 import IronIcon from '@mui/icons-material/Iron';
 
+// react icons
+// import { MdIron } from 'react-icons/fa'
+// import { GiDrop } from 'react-icons/fa'
+// import { GiHanger } from 'react-icons/fa'  
+// import { MdLocalLaundryService } from 'react-icons/fa'  
+
 
 function Card() {
   return (
@@ -12,14 +18,21 @@ function Card() {
       <div className=' '>
         <ul className='flex justify-evenly items-start '>
             <li className='flex flex-col justify-center items-center'>
-                <motion.div initial={{y: -50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 1}}><DryCleaningIcon  className='rounded-full h-[40px] w-[40px]  md:h-[64px] md:w-[64px] lg:h-[80px] lg:w-[80px] p-1  text-blue-500 my-2 bg-blue-200 hover:bg-blue-300 cursor-pointer hover:text-white' alt="" /></motion.div>
+                {/* <motion.div initial={{y: -50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 1}}><DryCleaningIcon  className='rounded-full h-[40px] w-[40px]  md:h-[64px] md:w-[64px] lg:h-[80px] lg:w-[80px] p-1  text-blue-500 my-2 bg-blue-200 hover:bg-blue-300 cursor-pointer hover:text-white' alt="" /></motion.div>
+                 */}
+
+                <DryCleaningIcon  className='rounded-full h-[40px] w-[40px]  md:h-[64px] md:w-[64px] lg:h-[80px] lg:w-[80px] p-1  text-blue-500 my-2 bg-blue-200 hover:bg-blue-300 cursor-pointer hover:text-white' alt="" />
                 <h3 className='text-[10px] md:text-2xl font-normal uppercase text-center mx-2'>Dry Cleaning</h3>
             </li>
 
             <span className="inline-block h-14 md:h-24 w-0.5 rounded bg-gray-100 mb-6"></span>
 
             <li className='flex flex-col justify-center items-center '>
+            {/* <motion.div initial={{y: -50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 1.3}}><OpacityIcon  className='rounded-full h-[40px] w-[40px]  md:h-[64px] md:w-[64px] lg:h-[80px] lg:w-[80px]  p-1 text-blue-500 my-2 bg-blue-200 hover:bg-blue-300 cursor-pointer hover:text-white' alt="" /></motion.div> */}
+
+
             <motion.div initial={{y: -50, opacity:0}} whileInView={{y:0, opacity:1}} transition={{duration: 1.3}}><OpacityIcon  className='rounded-full h-[40px] w-[40px]  md:h-[64px] md:w-[64px] lg:h-[80px] lg:w-[80px]  p-1 text-blue-500 my-2 bg-blue-200 hover:bg-blue-300 cursor-pointer hover:text-white' alt="" /></motion.div>
+
                 <h3 className='text-[10px] md:text-2xl font-normal uppercase text-center mx-2'>Wet Cleaning</h3>
             </li>
 
